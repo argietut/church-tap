6045cd41bac789a439fb2d6c8841c7c7b48f8540
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-03-29 11:31:04 +0800

    snackbar modification

--- lib/FRONT_END/MemberScreens/widget_member/Calendar.dart
+++ lib/FRONT_END/MemberScreens/widget_member/Calendar.dart
@@ -1,16 +1,12 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:bethel_app_final/BACK_END/Services/Functions/Authentication.dart';
 import 'package:bethel_app_final/BACK_END/Services/Functions/Users.dart';
 import 'package:bethel_app_final/FRONT_END/AdminScreens/church_event_source/add_event_church.dart';
-import 'package:bethel_app_final/FRONT_END/MemberScreens/appointment_page.dart';
 import 'package:bethel_app_final/FRONT_END/MemberScreens/screen_pages/appointment_source_directory/add_appointment.dart';
 import 'package:bethel_app_final/FRONT_END/constant/color.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
